4ac1d61b43a60c9d015a60b6e9928f94ded3dc95
C0LD <30940108+0xC0LD@users.noreply.github.com>
2019-04-26 19:04:47 +0200

    Add files via upload

---
+++ YouTubeThumb/YouTubeThumb.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace YouTubeThumb
+{
+    partial class YouTubeThumb
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(YouTubeThumb));
+            this.URL = new System.Windows.Forms.TextBox();
+            this.dl = new System.Windows.Forms.Button();
+            this.output = new System.Windows.Forms.TextBox();
+            this.HelpBallon = new System.Windows.Forms.ToolTip(this.components);
+            this.pb = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.save = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // URL
+            // 
+            this.URL.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.URL.BackColor = System.Drawing.Color.Black;
+            this.URL.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.URL.Font = new System.Drawing.Font("Consolas", 9F);
+            this.URL.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.URL.Location = new System.Drawing.Point(63, 12);
+            this.URL.Name = "URL";
+            this.URL.Size = new System.Drawing.Size(542, 22);
+            this.URL.TabIndex = 0;
+            this.URL.KeyDown += new System.Windows.Forms.KeyEventHandler(this.URL_KeyDown);
+            // 
+            // dl
+            // 
+            this.dl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.dl.BackColor = System.Drawing.Color.Black;
+            this.dl.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.dl.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.dl.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.dl.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.dl.Location = new System.Drawing.Point(611, 12);
+            this.dl.Name = "dl";
+            this.dl.Size = new System.Drawing.Size(61, 22);
+            this.dl.TabIndex = 1;
+            this.dl.Text = "Extract";
+            this.HelpBallon.SetToolTip(this.dl, "Download Thumbnail");
+            this.dl.UseVisualStyleBackColor = false;
+            this.dl.Click += new System.EventHandler(this.dl_Click);
+            // 
+            // output
+            // 
+            this.output.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.output.BackColor = System.Drawing.Color.Black;
+            this.output.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.output.Font = new System.Drawing.Font("Consolas", 9F);
+            this.output.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.output.Location = new System.Drawing.Point(12, 38);
+            this.output.Multiline = true;
+            this.output.Name = "output";
+            this.output.ReadOnly = true;
+            this.output.Size = new System.Drawing.Size(660, 128);
+            this.output.TabIndex = 2;
+            // 
+            // HelpBallon
+            // 
+            this.HelpBallon.BackColor = System.Drawing.Color.RoyalBlue;
+            this.HelpBallon.ForeColor = System.Drawing.Color.Black;
+            // 
+            // pb
+            // 
+            this.pb.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pb.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb.Location = new System.Drawing.Point(12, 172);
+            this.pb.Name = "pb";
+            this.pb.Size = new System.Drawing.Size(660, 378);
+            this.pb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb.TabIndex = 3;
+            this.pb.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 19);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "URL:";
+            // 
+            // save
+            // 
+            this.save.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.save.BackColor = System.Drawing.Color.Black;
+            this.save.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.save.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.save.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.save.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.save.Location = new System.Drawing.Point(629, 172);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(43, 22);
+            this.save.TabIndex = 5;
+            this.save.Text = "SAVE";
+            this.HelpBallon.SetToolTip(this.save, "Save Image");
+            this.save.UseVisualStyleBackColor = false;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // YouTubeThumb
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(684, 562);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pb);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.dl);
+            this.Controls.Add(this.URL);
+            this.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(350, 350);
+            this.Name = "YouTubeThumb";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Extract Youtube Thumbnail";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pb)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox URL;
+        private System.Windows.Forms.Button dl;
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.ToolTip HelpBallon;
+        private System.Windows.Forms.PictureBox pb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button save;
+    }
+}
+
---
+++ YouTubeThumb/YouTubeThumb.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Threading;
+using System.Drawing.Imaging;
+
+namespace YouTubeThumb
+{
+    public partial class YouTubeThumb : Form
+    {
+        public YouTubeThumb()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           URL.Focus();
+           this.ActiveControl = URL;
+        }
+
+        private bool GetThumbnail_Busy = false;
+        private void GetThumbnail_Busy_sw(bool busy)
+        {
+            if (busy == true)
+            {
+                GetThumbnail_Busy = true;
+
+                dl.Enabled = false;
+                URL.Enabled = false;
+
+                output.Text = "";
+                output.Focus();
+                ActiveControl = output;
+            }
+            else
+            {
+                GetThumbnail_Busy = false;
+
+                dl.Enabled = true;
+                URL.Enabled = true;
+            }
+        }
+        private void GetThumbnail_Thread(string url)
+        {
+            if (GetThumbnail_Busy == false)
+            {
+                new Thread(() => GetThumbnail(url)) { IsBackground =true }.Start();
+            }
+        }
+        private void GetThumbnail(string url)
+        {
+            GetThumbnail_Busy_sw(true);
+            
+            if (url.Contains("www.youtube.com"))
+            {
+                if (Uri.IsWellFormedUriString(url, UriKind.Absolute))
+                {
+                    //DOWNLOAD HTML
+                    output.Text += "DOWNLOADING HTML........: " + url + Environment.NewLine; ;
+
+                    byte[] html = null;
+
+                    try
+                    {
+                        html = new System.Net.WebClient().DownloadData(url);
+                    }
+                    catch (System.Net.WebException)
+                    {
+                        output.Text += "ERR: Forbidden." + Environment.NewLine;
+                        GetThumbnail_Busy_sw(false);
+                        return;
+                    }
+                    catch (ArgumentNullException)
+                    {
+                        output.Text += "ERR: null" + Environment.NewLine;
+                        GetThumbnail_Busy_sw(false);
+                        return;
+                    }
+
+                    output.Text += "DOWNLOADED HTML.........: " + ROund(html.Length) + " (" + html.Length + " bytes)" + Environment.NewLine; ;
+
+                    char[] htmlChars = System.Text.Encoding.Default.GetString(html).ToArray();
+
+                    output.Text += "SEARCHING HTML FOR THUMBNAIL URL..." + Environment.NewLine; ;
+
+                    //get urls like this: blablablablablablabla "some url we want" blablablablabla
+                    List<string> links = new List<string>();
+                    string link = "";
+                    bool afterQuote = false;
+                    foreach (char ch in htmlChars)
+                    {
+                        if (ch == '"')
+                        {
+                            afterQuote = !afterQuote;
+
+                            if (afterQuote == false)
+                            {
+                                links.Add(link);
+                                link = "";
+                            }
+                        }
+                        else if (afterQuote == true)
+                        {
+                            link = link + ch; //add chars to string after quote
+                        }
+                    }
+
+                    int dump = 0;
+                    int maxres_count = 0;
+                    string is_same_url = "";
+                    foreach (string l in links)
+                    {
+                        if (Uri.IsWellFormedUriString(l, UriKind.Absolute))
+                        {
+                            if (l.Contains("maxres"))
+                            {
+                                if (is_same_url != l)
+                                {
+                                    is_same_url = l;
+
+                                    string FileName = "maxres_" + maxres_count + ".jpg";
+
+                                    output.Text += "DOWNLOADING THUMBNAIL...: " + l + Environment.NewLine;
+                                    pb.Load(l);
+                                    output.Text += "DOWNLOADED THUMBNAIL....: " + l + Environment.NewLine; ;
+                                    maxres_count++;
+                                }
+                            }
+                            else
+                            {
+                                dump++;
+                            }
+                        }
+                        else
+                        {
+                            dump++;
+                        }
+                    }
+
+                    // output.Text += Environment.NewLine;
+                    // output.Text += "===[DEBUG]===" + Environment.NewLine;
+                    // output.Text += "dumped lines: " + dump + Environment.NewLine;
+                    // output.Text += "end;" + Environment.NewLine;
+
+                    GetThumbnail_Busy_sw(false);
+                    return;
+                }
+            }
+
+            output.Text += "Not a youtube link [" + url + "]" + Environment.NewLine;
+            GetThumbnail_Busy_sw(false);
+            return;
+        }
+        
+        private void dl_Click(object sender, EventArgs e)
+        {
+            GetThumbnail_Thread(URL.Text);
+        }
+
+        #region other
+
+        //other
+        private static string ROund(double roundMe)
+        {
+            // 1 Byte = 8 Bit
+            // 1 Kilobyte = 1024 Bytes
+            // 1 Megabyte = 1048576 Bytes
+            // 1 Gigabyte = 1073741824 Bytes
+            // 1 Terabyte = 1099511627776 Bytes
+
+            double roundMe_CONVERTED = 0;
+            string roundMe_CONVERTED_STRING = "";
+
+            if (roundMe > 1099511627776) //TB
+            {
+                roundMe_CONVERTED = roundMe / 1099511627776;
+                roundMe_CONVERTED_STRING = Math.Round(roundMe_CONVERTED, 2) + " TB";
+            }
+            else if (roundMe > 1073741824) //GB
+            {
+                roundMe_CONVERTED = roundMe / 1073741824;
+                roundMe_CONVERTED_STRING = Math.Round(roundMe_CONVERTED, 2) + " GB";
+            }
+            else if (roundMe > 1048576) //MB
+            {
+                roundMe_CONVERTED = roundMe / 1048576;
+                roundMe_CONVERTED_STRING = Math.Round(roundMe_CONVERTED, 2) + " MB";
+            }
+            else if (roundMe > 1024) //KB
+            {
+                roundMe_CONVERTED = roundMe / 1024;
+                roundMe_CONVERTED_STRING = Math.Round(roundMe_CONVERTED, 2) + " KB";
+            }
+
+            return roundMe_CONVERTED_STRING;
+        }
+        private static string GetTime()
+        {
+            TimeSpan diff = (new DateTime(2011, 02, 10) - new DateTime(2011, 02, 01));
+            return diff.TotalMilliseconds.ToString();
+        }
+
+        #endregion
+
+        private void URL_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                dl_Click(null, null);
+            }
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            if (pb.Image != null)
+            {
+                SaveFileDialog sfd = new SaveFileDialog() { Filter = "Images|*.png;*.bmp;*.jpg", FileName = "thumbnail" };
+
+                ImageFormat format = ImageFormat.Png;
+                if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    string ext = System.IO.Path.GetExtension(sfd.FileName);
+                    switch (ext)
+                    {
+                        case ".jpg":
+                            format = ImageFormat.Jpeg;
+                            break;
+                        case ".bmp":
+                            format = ImageFormat.Bmp;
+                            break;
+                    }
+                    pb.Image.Save(sfd.FileName, format);
+                }
+            }
+            else
+            {
+                output.Text += "No image has been downloaded (picture box is empty...)" + Environment.NewLine;
+            }
+        }
+    }
+}
